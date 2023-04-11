06b950c2796fb3f5a751680f1ebe0842c8768346
Matt Ford <matt@dancingfrog.co.uk>
2023-04-11 13:21:42 +0100

    Add a bot command handler

--- juxty/src/juxty/juxty_evolved.clj
+++ juxty/src/juxty/juxty_evolved.clj
@@ -1,1 +1,122 @@
-(ns juxty.juxty-evolved)
+(ns juxty.juxty-evolved
+  (:require
+   [clojure.string]))
+
+;; command handler
+(defn bot-found?
+  [state bot-id]
+  (boolean (get state bot-id)))
+
+(defn bot-not-found?
+  [state bot-id]
+  ((complement bot-found?) state bot-id))
+
+(defn get-bot-position
+  [state bot-id]
+  @(get-in state [bot-id :position]))
+
+(defn external-fail?
+  []
+  (-> (slurp (str "https://www.random.org/integers/?num=1&min=" 0
+                  "&max=" 1
+                  "&col=1&base=10&format=plain&rnd=new"))
+      clojure.string/trim
+      Integer.
+      (= 1)))
+
+(defn ->cmd-response
+  [m]
+  (merge 
+   {:cmd-response-id (random-uuid)
+    :created-at (System/currentTimeMillis)}
+   m))
+
+(defn ->bot-event
+  [event]
+  (merge {:event-id (random-uuid)
+          :created-at (System/currentTimeMillis)}
+         event))
+
+(defn produce-to!
+  [events event]
+  (swap! events conj event))
+
+(defn producer
+  [events]
+  (fn [event] 
+    (produce-to! events event)))
+
+(defn bot-cmd-handler
+  [cmd state producer]
+  (let [{:keys [type cmd-id bot-id]} cmd]
+    (if (external-fail?)
+      (->cmd-response {:status :failure
+                       :originating-cmd-id cmd-id
+                       :error [:external-service-says-no]})
+      (case type
+        :create
+        (if (bot-found? @state bot-id)
+          (->cmd-response {:status :failure
+                           :originating-cmd-id cmd-id
+                           :error [:bot-already-found bot-id]})
+          (when (producer (->bot-event {:type :creation
+                                        :bot-id bot-id
+                                        :position 0
+                                        :originating-cmd-id cmd-id}))
+            (->cmd-response {:status :success
+                             :originating-cmd-id cmd-id})))
+        :move-left
+        (cond
+          (bot-not-found? @state bot-id)
+          (->cmd-response {:status :failure
+                           :originating-cmd-id cmd-id
+                           :error [:bot-not-found bot-id]})
+          (<= (get-bot-position @state bot-id) -2)
+          (->cmd-response {:status :failure
+                           :originating-cmd-id cmd-id
+                           :error [:out-of-bounds :left]})
+          :else
+          (when (producer (->bot-event {:type :movement
+                                        :bot-id bot-id
+                                        :delta -1
+                                        :originating-cmd-id cmd-id}))
+            (->cmd-response {:status :success
+                             :originating-cmd-id cmd-id})))
+        :move-right
+        (cond
+          (bot-not-found? @state bot-id)
+          (->cmd-response {:status :failure
+                           :originating-cmd-id cmd-id
+                           :error [:bot-not-found bot-id]})            
+          (>= (get-bot-position @state bot-id) 2)
+          (->cmd-response {:status :failure
+                           :originating-cmd-id cmd-id
+                           :error [:out-of-bounds :right]})
+          :else
+          (when (producer (->bot-event {:type :movement
+                                        :bot-id bot-id
+                                        :delta 1
+                                        :originating-cmd-id cmd-id}))
+            (->cmd-response {:status :success
+                             :originating-cmd-id cmd-id})))))))
+
+(defn create-bot
+  [state {:keys [bot-id position created-at] :as _event}]
+  (swap! state assoc bot-id {:bot-id bot-id
+                             :position (atom position)
+                             :created-at created-at}))
+
+(defn move-bot
+  [state {:keys [bot-id delta] :as _event}]
+  (let [position (get-in @state [bot-id :position])]
+    (swap! position (fn [x] [+ x delta]))))
+
+(defn bot-event-handler
+  [event state]
+  (let [{:keys [type]} event]
+    (case type
+      :creation
+      (create-bot state event)
+      :movement
+      (move-bot state event))))
+
--- juxty/test/juxty/juxty_evolved_test.clj
+++ juxty/test/juxty/juxty_evolved_test.clj
@@ -1,4 +1,105 @@
 (ns juxty.juxty-evolved-test
   (:require [clojure.test :as t :refer [deftest is testing]]
-            [juxty.juxty-evolved :as sut :refer [left right ->Juxty]]))
+            [juxty.juxty-evolved :as sut :refer [bot-cmd-handler producer]]))
 
+(def bot-db (atom {:juxty {:bot-id :juxty
+                           :position (atom 0)
+                           :created-at 1681201515402}
+                   :cruxy {:bot-id :cruxy
+                           :position (atom -2)
+                           :created-at 1681201515983}
+                   :bluxy {:bot-id :bluxy
+                           :position (atom 2)
+                           :created-at 1681201516105}}))
+
+(deftest bot-cmd-handler-test
+  (with-redefs [sut/external-fail? (constantly false)]
+    (let [bot-events (atom [])
+          state bot-db
+          producer (producer bot-events)
+          run-cmd (fn [cmd] (-> cmd
+                                (bot-cmd-handler state producer)
+                                (dissoc :cmd-response-id :created-at)))]
+      (testing "create"
+        (is (= {:status :success
+                :originating-cmd-id 1000}
+               (run-cmd {:type :create
+                         :cmd-id 1000
+                         :bot-id :xtdby})))
+        (is (= {:status :failure
+                :originating-cmd-id 1001
+                :error [:bot-already-found :juxty]}
+               (run-cmd {:type :create
+                         :cmd-id 1001
+                         :bot-id :juxty})))
+        (is (= [{:type :creation
+                 :bot-id :xtdby
+                 :position 0
+                 :originating-cmd-id 1000}]
+               (mapv (fn [e] (dissoc e :event-id :created-at)) @bot-events))))
+      (testing "move-left"
+        (reset! bot-events [])
+        (is (= {:status :success
+                :originating-cmd-id 1000}
+               (run-cmd {:type :move-left
+                         :cmd-id 1000
+                         :bot-id :juxty})))
+        (is (= {:status :failure
+                :originating-cmd-id 1001
+                :error [:out-of-bounds :left]}
+               (run-cmd {:type :move-left
+                         :cmd-id 1001
+                         :bot-id :cruxy})))
+        (is (= [{:type :movement
+                 :bot-id :juxty
+                 :delta -1
+                 :originating-cmd-id 1000}]
+               (mapv (fn [e] (dissoc e :event-id :created-at)) @bot-events))))
+      (testing "move-right"
+        (reset! bot-events [])
+        (is (= {:status :success
+                :originating-cmd-id 1000}
+               (run-cmd {:type :move-right
+                         :cmd-id 1000
+                         :bot-id :juxty})))
+        (is (= {:status :failure
+                :originating-cmd-id 1001
+                :error [:out-of-bounds :right]}
+               (run-cmd {:type :move-right
+                         :cmd-id 1001
+                         :bot-id :bluxy})))
+        (is (= [{:type :movement
+                 :bot-id :juxty
+                 :delta 1
+                 :originating-cmd-id 1000}]
+               (mapv (fn [e] (dissoc e :event-id :created-at)) @bot-events))))
+      (testing "multiple-cmds"
+        (reset! bot-events [])
+        (is (= {:status :success
+                :originating-cmd-id 1000}
+               (run-cmd {:type :create
+                         :cmd-id 1000
+                         :bot-id :xtdby})))
+        (is (= {:status :success
+                :originating-cmd-id 1001}
+               (run-cmd {:type :move-right
+                         :cmd-id 1001
+                         :bot-id :juxty})))
+        (is (= {:status :success
+                :originating-cmd-id 1002}
+               (run-cmd {:type :move-right
+                         :cmd-id 1002
+                         :bot-id :juxty})))
+        (is (= [{:type :creation
+                 :bot-id :xtdby
+                 :position 0
+                 :originating-cmd-id 1000}
+                {:type :movement
+                 :bot-id :juxty
+                 :delta 1
+                 :originating-cmd-id 1001}
+                {:type :movement
+                 :bot-id :juxty
+                 :delta 1
+                 :originating-cmd-id 1002}]
+               (mapv (fn [e] (dissoc e :event-id :created-at)) @bot-events)))))))
