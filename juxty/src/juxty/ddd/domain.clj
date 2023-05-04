ee21e6ec7c7128923fe1a7515de73c0a2114182b
Matt Ford <matt@dancingfrog.co.uk>
2023-05-04 10:18:28 +0100

    Decouple get-by-id from methods as they should work over the bot map

--- juxty/src/juxty/ddd/domain.clj
+++ juxty/src/juxty/ddd/domain.clj
@@ -4,6 +4,7 @@
 ;; Repository
 (def repo (atom {:version 0
                  :aggregates {}}))
+
 (defn reset-repo! [] (reset! repo {:version 0
                                    :aggregates {}}))
 
@@ -12,46 +13,72 @@
   (get (:aggregates @repo) id))
 
 (defn select-keys-from-repo
-  "Returns in the first position the sequence of selected value objects/entities across all aggregates, and in
-  the second position the version of the repo at the time of retrieval."
+  "Returns in the first position the sequence of selected value objects/entities 
+  across all aggregates, and in the second position the version of the repo at the
+  time of retrieval."
   [keys]
   (let [repo @repo]
     [(for [val (vals (:aggregates repo))] (select-keys val keys))
      (:version repo)]))
 
 (defn save-to-repo [aggregate]
-  (let [id (:id aggregate)]
-    (cond
-      ;; create
-      (and (not (contains? (:aggregates @repo) id))
-           (== (:version @repo) (:created-at-repo-version aggregate)))
-      (swap! repo (fn [repo]
-                    (-> repo
-                        (assoc-in [:aggregates id] aggregate)
-                        (assoc :version (inc (:version repo))))))
-      ;; update
-      (contains? (:aggregates @repo) id)
-      (swap! repo assoc-in [:aggregates id] aggregate))))
+  (swap! repo
+         (fn [repo]
+           (let [id (:id aggregate)
+                 aggregates (:aggregates repo)
+                 repo-version (:version repo)
+                 aggregate-version (:version aggregate)]
+             (cond
+               ;; create
+               (and (some? aggregate)
+                    (not (contains? aggregates id))
+                    (== repo-version (:created-at-repo-version aggregate)))
+               (-> repo
+                   (assoc-in [:aggregates id] aggregate)
+                   (assoc :version (inc (:version repo))))
+               ;; update
+               (and (contains? aggregates id)
+                    (== (get-in aggregates [id :version]) (dec aggregate-version)))
+               (-> repo
+                   (assoc-in [:aggregates id] aggregate))
+               :else
+               repo)))))
 
 ;; Aggregates
 (s/def :bot/id uuid?)
 (s/def :bot/upper #{:arms :tentacles :manipulators})
 (s/def :bot/lower #{:wheels :tracks :legs})
+(s/def :bot/nickname string?)
+(s/def :bot/version number?)
 (s/def :bot/created-at-repo-version number?)
 (s/def :bot/aggregate
-  (s/keys :req-un [:bot/id :bot/upper :bot/lower]))
+  (s/keys :req-un [:bot/id :bot/upper :bot/lower
+                   :bot/version :bot/created-at-repo-version]
+          :opt-un [:bot/nickname]))
 
-(defn equal-body? [b1 b2]
+(defn- equal-body? [b1 b2]
   (and (= (:lower b1) (:lower b2))
        (= (:upper b1) (:upper b2))))
 
 (defn create-bot
+  "Constructor"
   [upper lower]
   (let [[bodies version] (select-keys-from-repo [:upper :lower])
         bot (s/conform :bot/aggregate {:id (random-uuid)
                                        :upper upper
                                        :lower lower
-                                       :created-at-repo-version version})]
+                                       :created-at-repo-version version
+                                       :version 0})]
     (when (and (not (s/invalid? bot))
-               (not-any? (partial equal-body? bot) bodies))
+               (not-any? (partial equal-body? bot) bodies)) ;; query to repo if body combination exists
+      bot)))
+
+
+(defn give-nickname
+  "Method"
+  [bot nickname] ;; bot = this
+  (let [bot (s/conform :bot/aggregate (some-> bot
+                                              (assoc :nickname nickname)
+                                              (update :version inc)))]
+    (when (not (s/invalid? bot)) 
       bot)))
--- juxty/test/juxty/ddd/domain_test.clj
+++ juxty/test/juxty/ddd/domain_test.clj
@@ -22,36 +22,40 @@
 
 (use-fixtures :each uuid-fixture repo-fixture)
 
-(deftest create-bot
+(deftest create-bot-test
   (is (= {:id (nth uuids 0)
           :upper :arms
           :lower :wheels
+          :version 0
           :created-at-repo-version 0}
          (sut/create-bot :arms :wheels)))
   (is (nil? (sut/create-bot :legs :wheels)))
   (is (nil? (sut/create-bot :legs :legs)))
   (is (nil? (sut/create-bot :foo :bar))))
 
-(deftest saving-valid-bots-to-repo
+(deftest saving-valid-bots-to-repo-test
   (sut/save-to-repo (sut/create-bot :arms :legs))           ;; 0
   (sut/save-to-repo (sut/create-bot :tentacles :tracks))    ;; 1
   (sut/save-to-repo (sut/create-bot :manipulators :wheels)) ;; 2
   (is (= {:aggregates {(nth uuids 0) {:id (nth uuids 0)
                                       :upper :arms
                                       :lower :legs
-                                      :created-at-repo-version 0}
+                                      :created-at-repo-version 0
+                                      :version 0}
                        (nth uuids 1) {:id (nth uuids 1)
                                       :upper :tentacles
                                       :lower :tracks
-                                      :created-at-repo-version 1}
+                                      :created-at-repo-version 1
+                                      :version 0}
                        (nth uuids 2) {:id (nth uuids 2)
                                       :upper :manipulators
                                       :lower :wheels
-                                      :created-at-repo-version 2}}
+                                      :created-at-repo-version 2
+                                      :version 0}}
           :version 3}
          @sut/repo)))
 
-(deftest checking-body-invariant-holds
+(deftest checking-body-invariant-holds-test
   (some-> (sut/create-bot :arms :legs)
           sut/save-to-repo)
   (is (nil? (some-> (sut/create-bot :arms :legs)
@@ -59,19 +63,57 @@
   (is (= {:aggregates {(nth uuids 0) {:id (nth uuids 0)
                                       :upper :arms
                                       :lower :legs
-                                      :created-at-repo-version 0}}
+                                      :created-at-repo-version 0
+                                      :version 0}}
           :version 1}
          @sut/repo)))
 
-(deftest consistency
-  (let [bot1 (sut/create-bot :arms :legs)
+(deftest give-nickname-test
+  (let [bot (sut/create-bot :arms :legs)]
+    (is (= {:id (nth uuids 0)
+            :upper :arms
+            :lower :legs
+            :created-at-repo-version 0
+            :nickname "Juxty"
+            :version 1}
+           (-> bot
+               (sut/give-nickname "Juxty"))))
+    (is (= {:id (nth uuids 0)
+            :upper :arms
+            :lower :legs
+            :created-at-repo-version 0
+            :nickname "Tripy"
+            :version 2}
+           (-> bot
+               (sut/give-nickname "Juxty")
+               (sut/give-nickname "Tripy"))))
+    (is (nil? (-> bot (sut/give-nickname 12345))))))
+
+(deftest creation-consistency-test
+  (let [bot1 (sut/create-bot :arms :legs) ;; different ids same body
         bot2 (sut/create-bot :arms :legs)]
     (sut/save-to-repo bot1)
-    (sut/save-to-repo bot2)
-    (sut/save-to-repo bot1)
+    (sut/save-to-repo bot2) ;; doesn't get saved
     (is (= {:aggregates {(nth uuids 0) {:id (nth uuids 0)
                                         :upper :arms
                                         :lower :legs
-                                        :created-at-repo-version 0}}
+                                        :created-at-repo-version 0
+                                        :version 0}}
+            :version 1}
+           @sut/repo))))
+
+(deftest updating-consistency-test
+  (let [bot0 (sut/create-bot :arms :legs)
+        bot1 (sut/give-nickname bot0 "Juxty")
+        bot2 (sut/give-nickname bot0 "XTDBY")]
+    (sut/save-to-repo bot0)
+    (sut/save-to-repo bot2)
+    (sut/save-to-repo bot1) ;; does nothing
+    (is (= {:aggregates {(nth uuids 0) {:id (nth uuids 0)
+                                        :upper :arms
+                                        :lower :legs
+                                        :nickname "XTDBY"
+                                        :created-at-repo-version 0
+                                        :version 1}}
             :version 1}
            @sut/repo))))
